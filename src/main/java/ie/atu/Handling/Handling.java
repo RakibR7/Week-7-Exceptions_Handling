d79ea0e98a2094b144ef8af0d1cd9ab1309175e9
Rakib <G00408644@atu.ie>
2022-11-07 09:55:08 +0000

    everything works. loops. try and catch added (working)

--- src/main/java/ie/atu/Handling/Handling.java
+++ src/main/java/ie/atu/Handling/Handling.java
@@ -1,21 +1,24 @@
 package ie.atu.Handling;
 import java.util.Scanner;
 
-
 public class Handling {
     public static void main(String[] args) {
-        System.out.println("please enter a number between 0-9");
-        Scanner myscan = new Scanner(System.in);
-        int mynum = myscan.nextInt();
+        for(int x=0;x<10;x++) {
+            try {
+                System.out.println("please enter a number between 0-9");
+                Scanner myscan = new Scanner(System.in);
+                int mynum = myscan.nextInt();
 
-        if (mynum <= 9 || mynum > 0){
-            System.out.println("You entered" + mynum);
 
-        }else
-        {
-            System.out.println("Not a valid number");
+                if (mynum <= 9 || mynum > 0) {
+                    System.out.println("You entered" + mynum);
+
+                } else {
+                    System.out.println("Not a valid number");
+                }
+            } catch (Exception e) {
+                System.out.println("Error Received, try again");
+            }
         }
-
-
     }
 }
