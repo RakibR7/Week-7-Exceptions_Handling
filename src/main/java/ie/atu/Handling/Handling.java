3540d2c7f87a912f4714f25609d796524d78200e
Rakib <G00408644@atu.ie>
2022-11-07 10:42:01 +0000

    Final code

--- src/main/java/ie/atu/Handling/Handling.java
+++ src/main/java/ie/atu/Handling/Handling.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Handling {
     public static void main(String[] args) {
-        for(int x=0;x<10;x++) {
+        int x =1;
+        while(true) {
             try {
                 System.out.println("please enter a number between 0-9");
                 Scanner myscan = new Scanner(System.in);
                 int mynum = myscan.nextInt();
-
 
                 if (mynum <= 9 && mynum > 0) {
                     System.out.println("You entered" + mynum);
@@ -17,12 +17,12 @@ public class Handling {
                 } else {
                     System.out.println("Not a valid number");
                 }
+                break;
+            } catch (InputMismatchException ime) {
+                System.out.println("Caught Exception");
+                //myscan.next();
             }
-            catch (InputMismatchException ime)
-            {
-                System.out.println("Caught Execption");
-
-            }
+            continue;
         }
     }
 }
